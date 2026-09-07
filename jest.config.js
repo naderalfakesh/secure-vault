7e7ff7616d00b339cccae977d9dfc5c4fa583270
Nadir Alfakesh <nader.alfakesh@gmail.com>
2026-09-07 12:52:42 +0400

    test: transform noble hashes in jest

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,11 @@ module.exports = {
   },
   resolver: 'react-native-worklets/jest/resolver.js',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // jest-expo's default list plus the ESM-only noble packages used for PIN hashing.
+  transformIgnorePatterns: [
+    '/node_modules/(?!(.pnpm|react-native|@react-native|@react-native-community|expo|@expo|@expo-google-fonts|react-navigation|@react-navigation|@sentry/react-native|native-base|standard-navigation|@noble))',
+    '/node_modules/react-native-reanimated/plugin/',
+    '/node_modules/@react-native/babel-preset/',
+  ],
   testPathIgnorePatterns: ['/node_modules/', '/ios/', '/android/', '/dist/'],
 };
